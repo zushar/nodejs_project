2b2c448a3ce5ddfb9630a21acf6c4e4658f2c86a
israel zusha rotenberg <100128949+zushar@users.noreply.github.com>
2024-03-05 14:08:43 +0200

    complete toysModel.js

---
+++ models/toysModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const joi = require('joi');
+const {string} = require("joi");
+
+const toysSchema = new mongoose.Schema({
+    name: String,
+    info: String,
+    category: String,
+    img_url: String,
+    price: Number,
+    user_id: String
+},{timestamps: true});
+exports.ToysModel = mongoose.model('toys', toysSchema);
+
+exports.validateToys = (_reqBody) => {
+    const schema = joi.object({
+        name: joi.string().min(2).max(100).required(),
+        info: joi.string().min(2).max(999).required(),
+        category: joi.string().min(2).max(100).required(),
+        img_url: joi.string().min(2).max(400).allow(null, ''),
+        price: joi.number().min(2).max(10000).required(),
+        user_id: joi.string().min(2).max(100).required()
+    });
+    return schema.validate(_reqBody);
+}
