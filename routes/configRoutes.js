1dfea55a948c02dd1da52a9ea3dd7d4fc94369dd
israel zusha rotenberg <100128949+zushar@users.noreply.github.com>
2024-03-15 15:01:50 +0200

    update the file

--- routes/configRoutes.js
+++ routes/configRoutes.js
@@ -4,4 +4,7 @@ const usersR = require('./users');
 exports.routesInit = (app) => {
     app.use('/toys', toysR);
     app.use('/users', usersR);
+    app.use((req, res) => {
+        res.status(404).json({ message: 'Resource not found. api documentation at localhost:port/public/api.html' });
+    });
     }
